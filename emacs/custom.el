dbc355e45e3f70dbbb370e20da89aae6336c617b
Simon Génier <simon.genier@protonmail.com>
2021-09-06 15:18:37 -0400

    Increase compatibility of linking script.

--- emacs/custom.el
+++ emacs/custom.el
@@ -4,10 +4,11 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(org-plus-contrib evil-cleverparens smartparens evil-collection vi-tilde-fringe vi-tilde-fringe-mode general evil-nerd-commenter evil-surround auctex merlin tuareg magit projectile evil-escape evil doom-themes embark-consult embark marginalia which-key rainbow-delimiters use-package selectrum-prescient doom-modeline consult)))
+   '(selectrum org-plus-contrib evil-cleverparens smartparens evil-collection vi-tilde-fringe vi-tilde-fringe-mode general evil-nerd-commenter evil-surround auctex merlin tuareg magit projectile evil-escape evil doom-themes embark-consult embark marginalia which-key rainbow-delimiters use-package selectrum-prescient doom-modeline consult)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(fixed-pitch ((t nil)))
+ '(font-latex-verbatim-face ((t (:inherit fixed-pitch :foreground "#a991f1" :slant normal)))))
