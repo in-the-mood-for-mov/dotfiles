6ed836039d7a1eb49edb397e6157ce7dd3814379
Simon Génier <simon.genier@protonmail.com>
2021-09-28 14:04:11 -0400

    Fix Ocaml support on Windows.

--- profile.ps1
+++ profile.ps1
@@ -3,12 +3,18 @@ $env:LANG = "en_CA.utf-8"
 Set-PSReadlineOption -EditMode Emacs -BellStyle Visual
 [Console]::TreatControlCAsInput = $True
 
-Invoke-Command {
-  $applicationData = [Environment]::GetFolderPath("ApplicationData")
+& {
+  function AddToPath {
+    param([String][Parameter(Mandatory, Position = 0)]$Path)
+    $env:PATH = "$Path$([IO.Path]::PathSeparator)$env:PATH"
+  }
+
+  $applicationData = [Environment]::GetFolderPath("LocalApplicationData")
 
   $env:RUSTUP_HOME = Join-Path $applicationData "rustup"
   $env:CARGO_HOME = Join-Path $applicationData "cargo"
-  $env:PATH = "$(Join-Path $env:CARGO_HOME "bin")$([IO.Path]::PathSeparator)$env:PATH"
+  AddToPath(Join-Path $env:CARGO_HOME "bin")
+  $env:OPAMROOT = Join-Path $applicationData "opam"
 }
 
 function Prompt {
@@ -24,7 +30,19 @@ Import-Module posh-git
 if (Get-Command "opam" -ErrorAction SilentlyContinue) {
   foreach ($line in (opam env --shell zsh)) {
     if ($line -match "^(?<var>\w+)='(?<value>.*)'") {
-      [Environment]::SetEnvironmentVariable($Matches.var, $Matches.value, "Process")
+      $var = $Matches.var
+      $value = if ($IsWindows -and $var -ieq "PATH") {
+        "$opamSwitchPrefix\bin;$env:PATH"
+      } else {
+        $Matches.value
+      }
+
+      if ($var -ieq "OPAM_SWITCH_PREFIX") {
+        $value = $value -replace '\\\\', '\'
+        $opamSwitchPrefix = $value
+      }
+
+      [Environment]::SetEnvironmentVariable($Matches.var, $value, "Process")
     }
   }
 }
